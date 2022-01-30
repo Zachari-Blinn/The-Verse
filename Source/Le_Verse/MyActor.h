3eed9fafe9befea8d396005038cecd9516a96768
Burei <rommanirs.mer@outlook.fr>
2022-01-30 21:30:18 +0100

    fix gitignore

---
+++ Source/Le_Verse/MyActor.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LE_VERSE_API MyActor
+{
+public:
+	MyActor();
+	~MyActor();
+};
